328aa3500136cdd908f834dade6504fe22cc725f
Brian D <dinhlol69@gmail.com>
2020-04-28 07:03:09 -0400

    Add HTML Routes

--- routes/HTMLRoute.js
+++ routes/HTMLRoute.js
@@ -7,4 +7,14 @@ module.exports = function (app) {
   app.get('/', (req, res) => {
     res.sendFile(path.join(PUBLIC_FOLDER_PATH, 'index.html'));
   });
+
+  // Notes Editor
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(PUBLIC_FOLDER_PATH, 'notes.html'));
+  });
+
+  // No Match -> Home Page
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(PUBLIC_FOLDER_PATH, 'index.html'));
+  })
 };
